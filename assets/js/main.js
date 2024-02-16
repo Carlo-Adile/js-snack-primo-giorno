6fbad48e21f10f557570a66ccdb40b4feef7eee3
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-16 15:38:16 +0100

    exercise 2 complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 /* exercise 1: 
-make a variable named number with value 100, then print the value on console  */
+make a variable named number with value 100, then print the value on console  
+*/
 let number= 100; //number
 console.log(number);
+
+/* exercise 2:
+make a variable PI along with a numerical value, then print it on console.
+*/
+const PI = 3.416;
+console.log(PI);
+
